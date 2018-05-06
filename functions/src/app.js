e77af8d0dcd07c19c9376292440708e16723d288
ampatspell <ampatspell@gmail.com>
2018-05-06 03:26:20 +0300

    add watch

--- functions/src/app.js
+++ functions/src/app.js
@@ -1,9 +1,11 @@
 import handlers from './handlers';
 
 export default class App {
+
   constructor(admin, functions) {
     this.admin = admin;
     this.functions = functions;
     this.handlers = handlers(this);
   }
+
 }
