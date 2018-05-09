3a4789f0352eaba9451dce92e85bb220c3eb3887
ampatspell <ampatspell@gmail.com>
2018-05-09 03:04:29 +0300

    todo

--- app/routes/sources/source.js
+++ app/routes/sources/source.js
@@ -5,6 +5,12 @@ export default Route.extend({
   async model(params) {
     let id = params.source_id;
     let doc = await this.store.sources.doc(id);
+    let collections = doc.ref.collection('collections').orderBy('name').query({ type: 'array' });
+    let observer = collections.observe();
+    console.log(observer);
+    // observer.query
+    // observer.cancel
+    // observer.promise
     return this.models.model('source', { doc }).load();
   }
 
