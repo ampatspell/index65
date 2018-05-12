88059943752628fb302b616757776e0a9c123134
ampatspell <ampatspell@gmail.com>
2018-05-12 09:05:47 +0300

    add left-right-escape to all screens

--- app/components/ui-route/sources/source/collections/collection/index/component.js
+++ app/components/ui-route/sources/source/collections/collection/index/component.js
@@ -1,8 +1,16 @@
 import Component from '@ember/component';
+import LeftRightMixin from 'index65/mixins/component/keyboard-left-right';
 
-export default Component.extend({
+export default Component.extend(LeftRightMixin, {
 
   actions: {
+    previous() {
+    },
+    next() {
+    },
+    escape() {
+      this.router.transitionTo('sources.source', this.source.id);
+    },
     group(doc) {
       this.router.transitionTo('sources.source.collections.collection.groups.group', doc.id);
     },
